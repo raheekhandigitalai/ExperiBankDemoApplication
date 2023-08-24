8f595c37117f077c21e35eab31069228b7fed917
Rahee Khan <71343050+raheekhandigitalai@users.noreply.github.com>
2023-08-24 16:07:46 -0400

    Update TestClass.java

--- src/test/java/tests/TestClass.java
+++ src/test/java/tests/TestClass.java
@@ -12,7 +12,7 @@ public class TestClass extends BaseClass {
 
     @Test
     public void makePaymentTest() throws InterruptedException {
-        wait.until(ExpectedConditions.elementToBeClickable(By.name("usernameTextField")));
+        getWait().until(ExpectedConditions.elementToBeClickable(By.name("usernameTextField")));
         getDriver().findElement(By.name("usernameTextField")).sendKeys("company");
         getDriver().findElement(By.name("passwordTextField")).sendKeys("company");
         getDriver().findElement(By.name("loginButton")).click();
@@ -24,7 +24,7 @@ public class TestClass extends BaseClass {
 
         getDriver().findElement(By.name("makePaymentButton")).click();
 
-        wait.until(ExpectedConditions.elementToBeClickable(By.name("cancelButton")));
+        getWait().until(ExpectedConditions.elementToBeClickable(By.name("cancelButton")));
         getDriver().findElement(By.name("cancelButton")).click();
         getDriver().findElement(By.name("logoutButton")).click();
 
