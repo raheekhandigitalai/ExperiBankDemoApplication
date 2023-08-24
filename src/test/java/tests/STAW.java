98a6d3453713f71dd6c7351bc14c6c17b24faff7
Rahee Khan <71343050+raheekhandigitalai@users.noreply.github.com>
2023-08-24 16:06:32 -0400

    Update STAW.java

--- src/test/java/tests/STAW.java
+++ src/test/java/tests/STAW.java
@@ -1,39 +1,39 @@
 package tests;
 
-import io.appium.java_client.ios.IOSDriver;
-import io.appium.java_client.ios.IOSElement;
-import io.appium.java_client.remote.MobileCapabilityType;
-import org.openqa.selenium.By;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+// import io.appium.java_client.ios.IOSDriver;
+// import io.appium.java_client.ios.IOSElement;
+// import io.appium.java_client.remote.MobileCapabilityType;
+// import org.openqa.selenium.By;
+// import org.openqa.selenium.remote.DesiredCapabilities;
+// import org.testng.annotations.AfterMethod;
+// import org.testng.annotations.BeforeMethod;
+// import org.testng.annotations.Test;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+// import java.net.MalformedURLException;
+// import java.net.URL;
 
 public class STAW {
 
-    private String accessKey = "";
-    private IOSDriver<IOSElement> driver = null;
-    DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+    // private String accessKey = "";
+    // private IOSDriver<IOSElement> driver = null;
+    // DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 
-    @BeforeMethod
-    public void setUp() throws MalformedURLException {
-        desiredCapabilities.setCapability("accessKey", accessKey);
-        desiredCapabilities.setCapability(MobileCapabilityType.UDID, "3cf62fb35d303b19b98d2b94e7ee06f582d2cf6a");
-        desiredCapabilities.setCapability("dontGoHomeOnQuit", true);
-        driver = new IOSDriver<>(new URL("https://uscloud.experitest.com/wd/hub"), desiredCapabilities);
-    }
+    // @BeforeMethod
+    // public void setUp() throws MalformedURLException {
+    //     desiredCapabilities.setCapability("accessKey", accessKey);
+    //     desiredCapabilities.setCapability(MobileCapabilityType.UDID, "3cf62fb35d303b19b98d2b94e7ee06f582d2cf6a");
+    //     desiredCapabilities.setCapability("dontGoHomeOnQuit", true);
+    //     driver = new IOSDriver<>(new URL("https://uscloud.experitest.com/wd/hub"), desiredCapabilities);
+    // }
 
-    @Test
-    public void testing() {
-        // Hello
-    }
+    // @Test
+    // public void testing() {
+    //     // Hello
+    // }
 
-    @AfterMethod
-    public void tearDown() {
-        driver.quit();
-    }
+    // @AfterMethod
+    // public void tearDown() {
+    //     driver.quit();
+    // }
 
 }
